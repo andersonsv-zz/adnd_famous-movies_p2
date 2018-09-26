29ecc06c391bf8afca656c0ab0f3c56e9d5692e9
Your Name <anderson.si.vieira@hotmail.com>
2018-09-25 22:37:57 -0300

    Inclusão de novo menu e Room

---
+++ app/src/main/java/br/com/andersonv/famousmovies/database/AppDatabase.java
@@ -0,0 +1,27 @@
+package br.com.andersonv.famousmovies.database;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.RoomDatabase;
+import android.support.annotation.NonNull;
+
+public class AppDatabase extends RoomDatabase{
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+}
