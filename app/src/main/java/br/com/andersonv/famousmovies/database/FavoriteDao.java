1db1510fa293ced90d76ef8bfb224256dfa860f6
Your Name <anderson.si.vieira@hotmail.com>
2018-09-30 20:18:40 -0300

    Implementação do Room

--- app/src/main/java/br/com/andersonv/famousmovies/database/FavoriteDao.java
+++ app/src/main/java/br/com/andersonv/famousmovies/database/FavoriteDao.java
@@ -1,4 +1,25 @@
 package br.com.andersonv.famousmovies.database;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface FavoriteDao {
+    @Query("SELECT * FROM favorite ORDER BY created_at")
+    LiveData<List<FavoriteEntry>> loadAllFavorites();
+
+    @Insert
+    void insertFavorite(FavoriteEntry favoriteEntry);
+    
+    @Delete
+    void deleteFavorite(FavoriteEntry favoriteEntry);
+
+    @Query("SELECT * FROM favorite WHERE id = :id")
+    LiveData<FavoriteEntry> loadFavoriteById(long id);
+
 }
--- app/src/main/java/br/com/andersonv/famousmovies/database/FavoriteEntry.java
+++ app/src/main/java/br/com/andersonv/famousmovies/database/FavoriteEntry.java
@@ -1,4 +1,73 @@
 package br.com.andersonv.famousmovies.database;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "favorite")
 public class FavoriteEntry {
+
+    @PrimaryKey
+    private long id;
+    private String title;
+    private String poster;
+    private String overview;
+    @ColumnInfo(name = "vote_average")
+    private Double voteAverage;
+
+    @ColumnInfo(name = "release_date")
+    private Date releaseDate;
+
+    @ColumnInfo(name = "created_at")
+    private Date createdAt;
+
+    @Ignore
+    public FavoriteEntry(String title, String poster, String overview, Double voteAverage, Date releaseDate, Date createdAt) {
+        this.title = title;
+        this.poster = poster;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.releaseDate = releaseDate;
+        this.createdAt = createdAt;
+    }
+
+    public FavoriteEntry(long id, String title, String poster, String overview, Double voteAverage, Date releaseDate, Date createdAt) {
+        this.id = id;
+        this.poster = poster;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.releaseDate = releaseDate;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 }
